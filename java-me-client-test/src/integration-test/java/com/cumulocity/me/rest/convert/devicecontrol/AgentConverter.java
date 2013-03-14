52af9ceb77f6eef15e2a3cdc21f22ebcc65e54d6
Dariusz Kaczyński <dariusz.kaczynski_ext@impaqgroup.com>
2013-03-14 14:35:24 +0100

    Moved test classes to a package without a capital letter in the name

--- java-me-client-test/src/integration-test/java/com/cumulocity/me/rest/convert/devicecontrol/AgentConverter.java
+++ java-me-client-test/src/integration-test/java/com/cumulocity/me/rest/convert/devicecontrol/AgentConverter.java
@@ -17,7 +17,7 @@
  * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package com.cumulocity.me.rest.convert.deviceControl;
+package com.cumulocity.me.rest.convert.devicecontrol;
 
 import com.cumulocity.me.rest.convert.base.BaseRepresentationConverter;
 import com.cumulocity.me.rest.json.JSONObject;
