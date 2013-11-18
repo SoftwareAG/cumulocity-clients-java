237fb45e886601a8365cff43d955ea7962bf2992
Lukasz Piasecki <Lukasz.Piasecki@impaqgroup.com>
2013-11-18 12:11:35 +0100

    Added test for encode param values

--- java-client/src/test/java/com/cumulocity/sdk/client/FilterTest.java
+++ java-client/src/test/java/com/cumulocity/sdk/client/FilterTest.java
@@ -6,9 +6,11 @@ import java.util.Map;
 
 import org.junit.Test;
 
+import com.cumulocity.model.DateConverter;
 import com.cumulocity.model.event.CumulocityAlarmStatuses;
 import com.cumulocity.model.idtype.GId;
 import com.cumulocity.sdk.client.alarm.AlarmFilter;
+import com.cumulocity.sdk.client.event.EventFilter;
 
 public class FilterTest {
 
@@ -19,5 +21,14 @@ public class FilterTest {
         Map<String, String> queryParams = alarmFilter.getQueryParams();
         
         assertThat(queryParams.keySet()).containsOnly("source","status");
+    }
+    
+    @Test
+    public void shouldEncodeParamValue() throws Exception {
+        EventFilter eventFilter = new EventFilter().byFromDate(DateConverter.string2Date("2011-11-03T11:01:00.000+05:30"));
+        
+        Map<String, String> queryParams = eventFilter.getQueryParams();
+
+        assertThat(queryParams.get("dateFrom")).isEqualTo("2011-11-03T06%3A31%3A00.000%2B01%3A00");
     }
 }
