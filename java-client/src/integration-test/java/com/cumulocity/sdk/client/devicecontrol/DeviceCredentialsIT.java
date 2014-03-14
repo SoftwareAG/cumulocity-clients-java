ab402266a2ebf2b71ebf65f8ef4402d47084f51a
Dominik Bielicki <dominik.bielicki@impaqgroup.com>
2014-03-14 18:43:54 +0100

    MTM-6310 extend java-client

---
+++ java-client/src/integration-test/java/com/cumulocity/sdk/client/devicecontrol/DeviceCredentialsIT.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2013 Cumulocity GmbH
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of 
+ * this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.cumulocity.sdk.client.devicecontrol;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.cumulocity.rest.representation.devicebootstrap.NewDeviceRequestRepresentation;
+import com.cumulocity.rest.representation.operation.DeviceControlMediaType;
+import com.cumulocity.sdk.client.ResponseParser;
+import com.cumulocity.sdk.client.common.HttpClientFactory;
+import com.cumulocity.sdk.client.common.JavaSdkITBase;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+public class DeviceCredentialsIT extends JavaSdkITBase {
+
+    private static final String NEW_DEVICE_REQUEST_URI = "devicecontrol/newDeviceRequests";
+    
+    private DeviceCredentialsApiImpl deviceCredentialsResource;
+    private ResponseParser responseParser = new ResponseParser();
+    
+    @Before
+    public void setUp() throws Exception {
+        deviceCredentialsResource = (DeviceCredentialsApiImpl) platform.getDeviceCredentialsApi();
+    }
+
+    @Test
+    public void hello() throws Exception {
+        createNewDeviceRequest("2000");
+        NewDeviceRequestRepresentation newDeviceRequest = getNewDeviceRequest("2000");
+        System.out.println(newDeviceRequest);
+        deviceCredentialsResource.hello("2000");
+    }
+    
+    public void createNewDeviceRequest(String deviceId) {
+        Client httpClient = new HttpClientFactory().createClient();
+        try {
+            ClientResponse response = postNewDeviceRequest(httpClient, deviceId);
+            assertThat(response.getStatus(), is(200));
+        } finally {
+            httpClient.destroy();
+        }
+    }
+    
+    public NewDeviceRequestRepresentation getNewDeviceRequest(String deviceId) {
+        Client httpClient = new HttpClientFactory().createClient();
+        try {
+            return getNewDeviceRequest(httpClient, deviceId);
+        } finally {
+            httpClient.destroy();
+        }
+    }
+    
+    private ClientResponse postNewDeviceRequest(Client httpClient, String deviceId) {
+        String host = platform.getHost();
+        WebResource.Builder resource = httpClient
+                .resource(host + NEW_DEVICE_REQUEST_URI)
+                .type(DeviceControlMediaType.NEW_DEVICE_REQUEST);
+        String json = "{ \"id\": \"" + deviceId + "\"}";
+        return resource.post(ClientResponse.class, json);
+    }
+    
+    private NewDeviceRequestRepresentation getNewDeviceRequest(Client httpClient, String deviceId) {
+        String host = platform.getHost();
+        WebResource.Builder resource = httpClient
+                .resource(host + NEW_DEVICE_REQUEST_URI + "/" + deviceId)
+                .type(DeviceControlMediaType.NEW_DEVICE_REQUEST);
+        ClientResponse clientResponse = resource.get(ClientResponse.class);
+        return responseParser.parse(clientResponse, 200, NewDeviceRequestRepresentation.class);
+    }
+
+
+}
