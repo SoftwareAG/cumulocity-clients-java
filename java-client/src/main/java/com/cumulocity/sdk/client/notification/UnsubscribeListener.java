2b7da526a857f73a78128d8a0d04432e5365a611
Jaroslaw Herod <jaroslaw.herod@cumulocity.com>
2014-03-21 12:28:16 +0100

    Code Review: removed obsolate classes, changed method names to be more informative

--- java-client/src/main/java/com/cumulocity/sdk/client/notification/UnsubscribeListener.java
+++
@@ -1,7 +0,0 @@
-package com.cumulocity.sdk.client.notification;
-
-import org.cometd.bayeux.client.ClientSessionChannel.ClientSessionChannelListener;
-
-public class UnsubscribeListener implements ClientSessionChannelListener {
-
-}
