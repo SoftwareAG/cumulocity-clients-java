e0dd993e788a1a123838552652e5a404b9a04077
Dominik Bielicki <dominik.bielicki@impaqgroup.com>
2014-07-15 14:03:05 +0200

    MTM-7054 add Measurement Request Operation for tixi

---
+++ device-capability-model/src/main/java/c8y/MeasurementRequestOperation.java
@@ -0,0 +1,23 @@
+package c8y;
+
+import java.io.Serializable;
+
+public class MeasurementRequestOperation implements Serializable {
+	
+    private static final long serialVersionUID = -2731997499381254447L;
+    
+	private String requestName;
+
+	public String getRequestName() {
+		return requestName;
+	}
+
+	public void setRequestName(String requestName) {
+		this.requestName = requestName;
+	}
+
+	@Override
+    public String toString() {
+	    return String.format("MeasurementRequestOperation [requestName=%s]", requestName);
+    }
+}
