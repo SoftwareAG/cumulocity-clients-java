10beb48ea5561fafeff6571bc461c1af2860fc82
Georgi Huklev <georgi.huklev@cumulocity.com>
2014-07-21 15:01:18 +0200

    small refactor

--- device-capability-model/src/main/java/c8y/MoistureMeasurement.java
+++ device-capability-model/src/main/java/c8y/MoistureMeasurement.java
@@ -8,25 +8,25 @@ import com.cumulocity.model.measurement.MeasurementValue;
 
 public class MoistureMeasurement {
 	public static final String MOISTURE_UNIT="%";
-	private MeasurementValue m;
+	private MeasurementValue moisture;
 	
-	@JSONProperty("M")
-	public MeasurementValue getM(){
-		return m;
+	@JSONProperty("moisture")
+	public MeasurementValue getMoisture(){
+		return moisture;
 	}
 	
-	public void setM(MeasurementValue m){
-		this.m = m;
+	public void setMoisture(MeasurementValue m){
+		this.moisture = m;
 	}
 	
 	@JSONProperty(ignore = true)
-	public BigDecimal getMoisutre(){
-		return m==null?null:m.getValue();
+	public BigDecimal getMoisutreValue(){
+		return moisture==null?null:moisture.getValue();
 	}
 	
-	public void setMoisture(BigDecimal moisture) {
-		m = new MeasurementValue(MOISTURE_UNIT);
-		m.setValue(moisture);
+	public void setMoistureValue(BigDecimal moistureValue) {
+		moisture = new MeasurementValue(MOISTURE_UNIT);
+		moisture.setValue(moistureValue);
 	}
 	
 	@Override
@@ -38,12 +38,12 @@ public class MoistureMeasurement {
 		if (!(obj instanceof TemperatureMeasurement))
 			return false;
 		
-		MoistureMeasurement moist = (MoistureMeasurement)obj;
-		return m==null?moist.m==null:m.equals(moist.m);
+		MoistureMeasurement mm = (MoistureMeasurement)obj;
+		return moisture==null?mm.moisture==null:moisture.equals(mm.moisture);
 	}
 	
 	@Override
 	public int hashCode() {
-		return m==null ? 0 : m.hashCode();
+		return moisture==null ? 0 : moisture.hashCode();
 	}
 }
