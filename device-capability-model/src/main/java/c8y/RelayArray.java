f71da79579aac66e08e4a0b1148c52d3e36421e6
Georgi Huklev <georgi.huklev@cumulocity.com>
2014-08-19 10:32:40 +0200

    Simplify class RelayArray

--- device-capability-model/src/main/java/c8y/RelayArray.java
+++ device-capability-model/src/main/java/c8y/RelayArray.java
@@ -1,41 +1,7 @@
 package c8y;
 
-public class RelayArray {
-	
-	private Relay relays[];
-	
-	public Relay[] getRelays(){
-		return relays;
-	}
-	
-	public void setRelays(Relay[] relays){
-		this.relays=relays;
-	}
-	
-	@Override
-	public int hashCode() {
-		return relays == null ? 0 : relays.hashCode();
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (obj == null)
-			return false;
-		if (obj == this)
-			return true;
-		if (!(obj instanceof RelayArray))
-			return false;
-		if (((RelayArray)obj).relays.length!=relays.length)
-			return false;
-		
-		for(int i=0; i<relays.length;i++) {
-			if(relays[i]==null) {
-				if(((RelayArray)obj).relays[i]!=null)
-					return false;
-			}
-			else if(relays[i].equals(((RelayArray)obj).relays[i]))
-				return false;
-		}
-		return true;
-	}
+import java.util.ArrayList;
+
+public class RelayArray extends ArrayList<Relay.RelayState> { 
+	private static final long serialVersionUID = -2380441943881612189L; 
 }
