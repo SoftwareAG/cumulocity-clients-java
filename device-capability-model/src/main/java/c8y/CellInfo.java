38dabeb1b6cee2232b3b08110ea16374a76abb14
Lukasz Piasecki <Lukasz.Piasecki@impaqgroup.com>
2015-04-13 13:26:03 +0200

    Created Inventory subscriber and added missing type for cell info

---
+++ device-capability-model/src/main/java/c8y/CellInfo.java
@@ -0,0 +1,61 @@
+package c8y;
+
+import java.util.List;
+
+public class CellInfo {
+
+    private String radioType;
+    private List<CellTower> cellTowers;
+    
+    public CellInfo() {
+        
+    }
+    
+    public CellInfo(String radioType, List<CellTower> cellTowers) {
+        this.radioType = radioType;
+        this.cellTowers = cellTowers;
+    }
+    public String getRadioType() {
+        return radioType;
+    }
+    public void setRadioType(String radioType) {
+        this.radioType = radioType;
+    }
+    public List<CellTower> getCellTowers() {
+        return cellTowers;
+    }
+    public void setCellTowers(List<CellTower> cellTowers) {
+        this.cellTowers = cellTowers;
+    }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((cellTowers == null) ? 0 : cellTowers.hashCode());
+        result = prime * result + ((radioType == null) ? 0 : radioType.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CellInfo other = (CellInfo) obj;
+        if (cellTowers == null) {
+            if (other.cellTowers != null)
+                return false;
+        } else if (!cellTowers.equals(other.cellTowers))
+            return false;
+        if (radioType == null) {
+            if (other.radioType != null)
+                return false;
+        } else if (!radioType.equals(other.radioType))
+            return false;
+        return true;
+    }
+
+}
