8236187c722fc7aa91f1099f22f8ff1b66e56053
Tobias Sommer <tobias.sommer@cumulocity.com>
2015-12-11 16:07:14 +0100

    adding binaries api to java client

---
+++ java-client/src/main/java/com/cumulocity/sdk/client/inventory/BinariesApiImpl.java
@@ -0,0 +1,45 @@
+package com.cumulocity.sdk.client.inventory;
+
+import java.io.File;
+import java.io.InputStream;
+
+import javax.ws.rs.core.MediaType;
+
+import com.cumulocity.model.idtype.GId;
+import com.cumulocity.rest.representation.inventory.InventoryMediaType;
+import com.cumulocity.rest.representation.inventory.InventoryRepresentation;
+import com.cumulocity.rest.representation.inventory.ManagedObjectRepresentation;
+import com.cumulocity.sdk.client.RestConnector;
+import com.cumulocity.sdk.client.SDKException;
+
+public class BinariesApiImpl implements BinariesApi {
+
+    private final RestConnector restConnector;
+
+    private InventoryRepresentation inventoryRepresentation;
+    
+    public BinariesApiImpl(RestConnector restConnector, InventoryRepresentation inventoryRepresentation) {
+        this.restConnector = restConnector;
+        this.inventoryRepresentation = inventoryRepresentation;
+    }
+
+    @Override
+    public ManagedObjectRepresentation uploadFile(ManagedObjectRepresentation container, byte[] bytes) throws SDKException {
+        return restConnector.postFile(getBinariesUrl(), container, bytes, ManagedObjectRepresentation.class);
+    }
+
+    @Override
+    public ManagedObjectRepresentation replaceFile(GId containerId, String contentType, InputStream fileStream) throws SDKException {
+        return restConnector.putStream(getBinariesUrl() + "/" + containerId.getValue(), contentType, fileStream, ManagedObjectRepresentation.class);
+    }
+
+    @Override
+    public void deleteFile(GId containerId) throws SDKException {
+        restConnector.delete(getBinariesUrl() + "/" + containerId.getValue());
+    }
+    
+    private String getBinariesUrl() throws SDKException {
+        String url = inventoryRepresentation.getManagedObjects().getSelf();
+        return url.replace("managedObjects", "binaries");
+    }
+}
