6738c733a1e5b3a91c0fd3b2eb0015e99a5df00e
Wojciech Trzcinski <Wojciech.Trzcinski@impaqgroup.com>
2015-12-17 10:11:21 +0100

    MTM-9321 Devices with illegal credentials cause high traffic load and fill up log storage
    Renamed SystemOptionRepository to AuthenticationRepository

--- agent-framework/repositories/src/main/java/com/cumulocity/agent/server/repository/AuthenticationRepository.java
+++ agent-framework/repositories/src/main/java/com/cumulocity/agent/server/repository/AuthenticationRepository.java
@@ -10,7 +10,7 @@ import javax.ws.rs.core.Response;
 import static com.cumulocity.rest.representation.tenant.OptionMediaType.OPTION_COLLECTION;
 
 @Component
-public class SystemOptionRepository {
+public class AuthenticationRepository {
 
     public static final String SYSTEM_OPTION_COLLECTION_RESOURCE_URL = "/tenant/system/options";
 
@@ -18,12 +18,12 @@ public class SystemOptionRepository {
     private final String baseUrl;
 
     @Autowired
-    public SystemOptionRepository(RestConnector rest, @Value("${C8Y.baseURL}") String baseUrl) {
+    public AuthenticationRepository(RestConnector rest, @Value("${C8Y.baseURL}") String baseUrl) {
         this.rest = rest;
         this.baseUrl = baseUrl;
     }
 
-    public boolean exists() {
+    public boolean isAuthenticated() {
         return rest.getStatus(url(), OPTION_COLLECTION) == Response.Status.OK;
     }
 
