fd29aa63f5b5ebd4818096c7c46ffdd6e4441961
Dominik Bielicki <dominik.bielicki@impaqgroup.com>
2016-01-28 23:03:33 +0100

    add tracker agent fragments due to trackerando rfv16 commands support

--- device-capability-model/src/main/java/c8y/SetSosNumber.java
+++ device-capability-model/src/main/java/c8y/SetSosNumber.java
@@ -8,82 +8,50 @@ public class SetSosNumber extends AbstractDynamicProperties implements Serializa
 
     private static final long serialVersionUID = 1608763081931360921L;
 
-    private String phoneNumber1;
-    private String phoneNumber2;
-    private String phoneNumber3;
-    
+    private String phoneNumber;
+
     public SetSosNumber() {}
-    
-    public SetSosNumber(String phoneNumber1, String phoneNumber2, String phoneNumber3) {
-	this.phoneNumber1 = phoneNumber1;
-	this.phoneNumber2 = phoneNumber2;
-	this.phoneNumber3 = phoneNumber3;
+
+    public SetSosNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
-    public String getPhoneNumber1() {
-	return phoneNumber1;
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
 
-    public void setPhoneNumber1(String phoneNumber1) {
-	this.phoneNumber1 = phoneNumber1;
-    }
-
-    public String getPhoneNumber2() {
-	return phoneNumber2;
-    }
-
-    public void setPhoneNumber2(String phoneNumber2) {
-	this.phoneNumber2 = phoneNumber2;
-    }
-
-    public String getPhoneNumber3() {
-	return phoneNumber3;
-    }
-
-    public void setPhoneNumber3(String phoneNumber3) {
-	this.phoneNumber3 = phoneNumber3;
-    }
-
-    @Override
-    public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + ((phoneNumber1 == null) ? 0 : phoneNumber1.hashCode());
-	result = prime * result + ((phoneNumber2 == null) ? 0 : phoneNumber2.hashCode());
-	result = prime * result + ((phoneNumber3 == null) ? 0 : phoneNumber3.hashCode());
-	return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-	if (this == obj)
-	    return true;
-	if (obj == null)
-	    return false;
-	if (getClass() != obj.getClass())
-	    return false;
-	SetSosNumber other = (SetSosNumber) obj;
-	if (phoneNumber1 == null) {
-	    if (other.phoneNumber1 != null)
-		return false;
-	} else if (!phoneNumber1.equals(other.phoneNumber1))
-	    return false;
-	if (phoneNumber2 == null) {
-	    if (other.phoneNumber2 != null)
-		return false;
-	} else if (!phoneNumber2.equals(other.phoneNumber2))
-	    return false;
-	if (phoneNumber3 == null) {
-	    if (other.phoneNumber3 != null)
-		return false;
-	} else if (!phoneNumber3.equals(other.phoneNumber3))
-	    return false;
-	return true;
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
     @Override
     public String toString() {
-	return "SetSosNumber [phoneNumber1=" + phoneNumber1 + ", phoneNumber2=" + phoneNumber2 + ", phoneNumber3=" + phoneNumber3 + "]";
+        return "SetSosNumber [phoneNumber=" + phoneNumber + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((phoneNumber == null) ? 0 : phoneNumber.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        SetSosNumber other = (SetSosNumber) obj;
+        if (phoneNumber == null) {
+            if (other.phoneNumber != null)
+                return false;
+        } else if (!phoneNumber.equals(other.phoneNumber))
+            return false;
+        return true;
     }
 
 }
