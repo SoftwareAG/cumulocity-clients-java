58f4cc3e8cd7c82467042ac48c7d7fb25a869efc
Ozge-Akin <ozge.akin@cumulocity.com>
2016-10-10 10:32:55 +0200

    equals method added to Tracking class

--- device-capability-model/src/main/java/c8y/Tracking.java
+++ device-capability-model/src/main/java/c8y/Tracking.java
@@ -14,4 +14,17 @@ public class Tracking extends AbstractDynamicProperties {
     public void setInterval(int interval) {
         this.interval = interval;
     }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (obj == this)
+            return true;
+        if (!(obj instanceof MotionTracking))
+            return false;
+        
+        Tracking rhs = (Tracking) obj;
+        return interval == rhs.interval;
+    }
 }
