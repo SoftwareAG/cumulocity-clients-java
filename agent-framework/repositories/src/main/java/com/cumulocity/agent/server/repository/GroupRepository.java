91480fd0e6170aa31c2fbc103fe39b907ea21f28
Lukasz Piasecki <Lukasz.Piasecki@impaqgroup.com>
2016-10-14 11:32:54 +0200

    extended user repositories in agent framework

--- agent-framework/repositories/src/main/java/com/cumulocity/agent/server/repository/GroupRepository.java
+++ agent-framework/repositories/src/main/java/com/cumulocity/agent/server/repository/GroupRepository.java
@@ -6,15 +6,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.cumulocity.agent.server.context.DeviceContextService;
+import com.cumulocity.rest.representation.user.GroupRepresentation;
 import com.cumulocity.rest.representation.user.UserMediaType;
-import com.cumulocity.rest.representation.user.UserRepresentation;
+import com.cumulocity.rest.representation.user.UserReferenceRepresentation;
 import com.cumulocity.sdk.client.RestConnector;
 import com.cumulocity.sdk.client.SDKException;
 
 @Component
-public class UserRepository {
+public class GroupRepository {
 
-    private static final String USER_URL = "user/{tenant}/users/";
+    private static final String USER_URL = "user/{tenant}/";
 
     private final RestConnector restConnector;
     
@@ -23,7 +24,7 @@ public class UserRepository {
     private final String baseUrl;
 
     @Autowired
-    public UserRepository(RestConnector restConnector, @Value("${C8Y.baseURL}") String host, DeviceContextService contextService) {
+    public GroupRepository(RestConnector restConnector, @Value("${C8Y.baseURL}") String host, DeviceContextService contextService) {
         this.restConnector = restConnector;
         this.contextService = contextService;
         if (host.charAt(host.length() - 1) != '/') {
@@ -32,10 +33,11 @@ public class UserRepository {
         this.baseUrl = host;
     }
 
-    public UserRepresentation findByUsername(String username) {
+    public GroupRepresentation findGroupByName(String name) {
         String userApiWithTenantURL = USER_URL.replace("{tenant}", contextService.getContext().getLogin().getTenant());
+        String groupByNameURL = userApiWithTenantURL + "groupByName/";
         try {
-            return restConnector.get(baseUrl + userApiWithTenantURL + username, UserMediaType.USER, UserRepresentation.class);
+            return restConnector.get(baseUrl + groupByNameURL + name, UserMediaType.GROUP, GroupRepresentation.class);
         } catch (SDKException ex) {
             if (ex.getHttpStatus() == HttpStatus.SC_NOT_FOUND) {
                 return null;
@@ -44,14 +46,18 @@ public class UserRepository {
             }
         }
     }
-    
-    public UserRepresentation create(UserRepresentation user) {
+
+    public GroupRepresentation create(GroupRepresentation groupRepresentation) {
         String userApiWithTenantURL = USER_URL.replace("{tenant}", contextService.getContext().getLogin().getTenant());
-        return restConnector.post(baseUrl + userApiWithTenantURL, UserMediaType.USER, user);
+        String groupsURL = userApiWithTenantURL + "groups/";
+        return restConnector.get(baseUrl + groupsURL, UserMediaType.GROUP, GroupRepresentation.class);
     }
-    
-    public void delete(String username) {
+
+    public void addUserToGroup(String username, Long id) {
         String userApiWithTenantURL = USER_URL.replace("{tenant}", contextService.getContext().getLogin().getTenant());
-        restConnector.delete(baseUrl + userApiWithTenantURL + username);
+        String groupMembersURL = userApiWithTenantURL + "groups/" + id + "/users";
+        UserReferenceRepresentation userReferenceRepresentation = new UserReferenceRepresentation();
+        userReferenceRepresentation.setSelf("/users" + username);
+        restConnector.post(baseUrl + groupMembersURL, UserMediaType.USER_REFERENCE, null);
     }
 }
