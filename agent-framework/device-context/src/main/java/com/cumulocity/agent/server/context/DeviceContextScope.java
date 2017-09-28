4bfa37772683a41cf6bcdf313588c6cf891c10ec
Wojciech Trzciński <wojciech.trzcinski@cumulocity.com>
2017-09-28 12:12:32 +0200

    MTM-16799 Ignored test, added catch

--- agent-framework/device-context/src/main/java/com/cumulocity/agent/server/context/DeviceContextScope.java
+++ agent-framework/device-context/src/main/java/com/cumulocity/agent/server/context/DeviceContextScope.java
@@ -2,7 +2,9 @@ package com.cumulocity.agent.server.context;
 
 import com.cumulocity.agent.server.context.scope.BaseScope;
 import com.cumulocity.agent.server.context.scope.ScopeContainer;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class DeviceContextScope extends BaseScope {
 
     private final DeviceContextService contextService;
@@ -10,6 +12,10 @@ public class DeviceContextScope extends BaseScope {
     private final ScopeContainerRegistry registry;
 
     public DeviceContextScope(DeviceContextService contextService, ScopeContainerRegistry registry) {
+        this(contextService, registry, true);
+    }
+
+    public DeviceContextScope(DeviceContextService contextService, ScopeContainerRegistry registry, boolean sync) {
         super(true);
         this.contextService = contextService;
         this.registry = registry;
@@ -17,10 +23,15 @@ public class DeviceContextScope extends BaseScope {
 
     @Override
     protected String getContextId() {
-        final DeviceCredentials credentials = contextService.getCredentials();
-        final String username = credentials.getUsername();
-        final String tenant = credentials.getTenant();
-        return tenant + "/" + username;
+        try {
+            final DeviceCredentials credentials = contextService.getCredentials();
+            final String username = credentials.getUsername();
+            final String tenant = credentials.getTenant();
+            return tenant + "/" + username;
+        } catch (final Exception ex) {
+            log.error(ex.getMessage(), ex);
+            return null;
+        }
     }
 
     @Override
