279ad9272ee5a3b414216910d1c7855bdb09d4e3
Slawomir Chmiel <slawomir.chmiel@cumulocity.com>
2017-10-16 14:32:03 +0200

    MTM-17271 switch to maven wagon

--- agent-framework/package/src/main/java/com/cumulocity/agent/packaging/microservice/MicroserviceDeployMojo.java
+++ agent-framework/package/src/main/java/com/cumulocity/agent/packaging/microservice/MicroserviceDeployMojo.java
@@ -1,31 +1,32 @@
 package com.cumulocity.agent.packaging.microservice;
 
-import org.apache.commons.lang.StringUtils;
-import org.apache.http.HttpHeaders;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.RequestBuilder;
-import org.apache.http.entity.mime.MultipartEntityBuilder;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+import com.google.common.base.Optional;
+import org.apache.maven.artifact.manager.WagonManager;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.settings.Server;
+import org.apache.maven.settings.Settings;
+import org.apache.maven.wagon.Wagon;
+import org.apache.maven.wagon.repository.Repository;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 
 import static com.cumulocity.agent.packaging.microservice.MicroservicePackageMojo.TARGET_FILENAME_PATTERN;
 import static com.google.common.base.Throwables.propagate;
-import static org.apache.http.entity.ContentType.APPLICATION_OCTET_STREAM;
+import static java.lang.String.format;
 import static org.apache.maven.plugins.annotations.LifecyclePhase.DEPLOY;
 
 @Mojo(name = "microservice-deploy", defaultPhase = DEPLOY)
 public class MicroserviceDeployMojo extends AbstractMojo {
 
+    private static final String SERVER_ID = "microservice";
+    
     @Parameter(defaultValue = "${project.build.directory}")
     private File build;
     @Parameter(defaultValue = "${project}", readonly = true)
@@ -33,10 +34,11 @@ public class MicroserviceDeployMojo extends AbstractMojo {
 
     @Parameter(defaultValue = "true", property = "skip.microservice.deploy")
     private boolean skip;
-    @Parameter
-    private String authorizationHeader;
-    @Parameter
-    private String deployUrl;
+
+    @Component
+    private WagonManager wagonManager;
+    @Component
+    private Settings settings;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
@@ -44,29 +46,36 @@ public class MicroserviceDeployMojo extends AbstractMojo {
             getLog().info("skipping microservice package deployment");
             return;
         }
-        if (StringUtils.isBlank(deployUrl)) {
-            getLog().warn("URL parameter was not configured, microservice package cannot be deployed");
+        final Optional<String> serverUrl = getServerUrl();
+        if (!serverUrl.isPresent()) {
+            getLog().warn(format("URL parameter was not configured, microservice package cannot be deployed. please check your '%s' server configuration in settings.xml", SERVER_ID));
             return;
         }
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            final String targetFilename = String.format(TARGET_FILENAME_PATTERN, project.getArtifactId(), project.getVersion());
-            final File file = new File(build, targetFilename);
-            final RequestBuilder requestBuilder = RequestBuilder.post(deployUrl)
-                    .setEntity(MultipartEntityBuilder.create()
-                            .addBinaryBody("file", new FileInputStream(file), APPLICATION_OCTET_STREAM, file.getName())
-                            .build());
-            if (StringUtils.isNotBlank(authorizationHeader)) {
-                requestBuilder.addHeader(HttpHeaders.AUTHORIZATION, authorizationHeader);
-            }
 
-            CloseableHttpResponse response = httpClient.execute(requestBuilder.build());
-            int responseCode = response.getStatusLine().getStatusCode();
-            if (responseCode < 200 || responseCode >= 300) {
-                throw new MojoExecutionException(String.format("Microservice package was not deployed properly, response details: %s", response));
-            }
-        } catch (IOException e) {
+        final String targetFilename = format(TARGET_FILENAME_PATTERN, project.getArtifactId(), project.getVersion());
+        final File file = new File(build, targetFilename);
+        try {
+            final Repository repository = new Repository(SERVER_ID, serverUrl.get());
+            final Wagon wagon = wagonManager.getWagon(repository);
+            wagon.connect(repository, wagonManager.getAuthenticationInfo(SERVER_ID));
+            wagon.put(file, targetFilename);
+            wagon.disconnect();
+        } catch (Exception e) {
             propagate(e);
         }
+    }
+
+    private Optional<String> getServerUrl() {
+        final Server server = settings.getServer(SERVER_ID);
+        final Object configuration = server.getConfiguration();
+        if (!(configuration instanceof Xpp3Dom)) {
+            return Optional.absent();
+        }
+        final Xpp3Dom urlDom = ((Xpp3Dom) configuration).getChild("url");
+        if (urlDom == null) {
+            return Optional.absent();
+        }
+        return Optional.of(urlDom.getValue());
     }
 
 }
