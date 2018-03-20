fd36c506e1416f2ebc4f99a38dd953c2f35e8a90
Dariusz Dudziński <dariusz.dudzinski@cumulocity.com>
2018-03-20 09:35:09 +0100

    remove microservice *InternalApi

--- microservice/api/src/main/java/com/cumulocity/microservice/platform/api/annotation/EnableMicroservicePlatformInternalApi.java
+++ microservice/api/src/main/java/com/cumulocity/microservice/platform/api/annotation/EnableMicroservicePlatformInternalApi.java
@@ -5,9 +5,7 @@ import org.springframework.context.annotation.Import;
 import java.lang.annotation.*;
 
 /**
- * 
- * Indicates that usage of internal Cumulocity APIs are enabled.
- * Internal Cumulocity API requests do not increase the request counts unlike regular Cumulocity API requests.
+ *  Prepares environment to work with platform api. Injects basic beans for comuunication with the platform.
  *
  */
 @Target(ElementType.TYPE)
