1d8798d4442e17502abef5b398e45c31cc801860
Wojciech Trzciński <wojciech.trzcinski@cumulocity.com>
2018-06-07 09:42:48 +0200

    MTM-20233 Sms lib backwards compatibility

--- java-sms-client/src/main/java/com/cumulocity/sms/client/SmsMessagingApiImpl.java
+++ java-sms-client/src/main/java/com/cumulocity/sms/client/SmsMessagingApiImpl.java
@@ -4,19 +4,42 @@ import com.cumulocity.model.sms.*;
 import com.cumulocity.sms.client.messaging.MessagingClient;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.fluent.Executor;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+
+import java.net.URI;
 
 import static com.cumulocity.sms.client.messaging.MessagingClient.addLeadingSlash;
+import static org.apache.http.client.fluent.Executor.newInstance;
 
 @Slf4j
 @Getter
 public class SmsMessagingApiImpl implements SmsMessagingApi {
 
+    // used directly by tracker agent
+    public static abstract class SmsCredentialsProvider {
+        public abstract String getTenant();
+
+        public abstract String getUsername();
+
+        public abstract String getPassword();
+    }
+
     private final MessagingClient messagingClient;
 
     public SmsMessagingApiImpl(final String host, Executor executor) {
-        final String url = addLeadingSlash(host) + "service/messaging/smsmessaging/v1";
+        final String url = concat(host, "service/messaging/smsmessaging/v1");
         messagingClient = new MessagingClient(url, executor);
+    }
+
+    // used directly by tracker agent
+    public SmsMessagingApiImpl(final String host, String contextPath, SmsCredentialsProvider executor) {
+        final String url = concat(host, contextPath);
+        messagingClient = new MessagingClient(url, createExecutor(url, executor));
     }
 
     @Override
@@ -42,5 +65,24 @@ public class SmsMessagingApiImpl implements SmsMessagingApi {
             throw new NullPointerException("Receive address and message id can not be null");
         }
         return messagingClient.getMessage(receiveAddress, messageId);
+    }
+
+    private String concat(String host, String contextPath) {
+        return addLeadingSlash(host) + contextPath;
+    }
+
+    private static Executor createExecutor(final String url, final SmsCredentialsProvider auth) {
+        return newInstance()
+                .use(new BasicCredentialsProvider() {
+                    public Credentials getCredentials(AuthScope authscope) {
+                        return new UsernamePasswordCredentials(auth.getTenant() + "/" + auth.getUsername(), auth.getPassword());
+                    }
+                })
+                .authPreemptive(getHost(url));
+    }
+
+    private static HttpHost getHost(String host) {
+        final URI uri = URI.create(host);
+        return new HttpHost(uri.getHost(), uri.getPort());
     }
 }
