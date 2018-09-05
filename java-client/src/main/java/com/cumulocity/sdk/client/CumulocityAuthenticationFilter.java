fe06a0e97be687f6e0af874b08d0d5c2c89aa912
Arkadiusz Borek <arkadiusz.borek@cumulocity.com>
2018-09-05 10:41:22 +0200

    MTM-22279 - add possibility to use CumulocityAuthenticationFilter.java without Cumulocity context.

--- java-client/src/main/java/com/cumulocity/sdk/client/CumulocityAuthenticationFilter.java
+++ java-client/src/main/java/com/cumulocity/sdk/client/CumulocityAuthenticationFilter.java
@@ -6,10 +6,12 @@ import com.sun.jersey.api.client.ClientRequest;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.filter.ClientFilter;
 import com.sun.jersey.core.util.Base64;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.ws.rs.core.HttpHeaders;
 import java.io.UnsupportedEncodingException;
 
+@Slf4j
 class CumulocityAuthenticationFilter extends ClientFilter {
 
     private final String authentication;
@@ -38,17 +40,45 @@ class CumulocityAuthenticationFilter extends ClientFilter {
     @Override
     public ClientResponse handle(final ClientRequest cr) throws ClientHandlerException {
 
-        if (oAuthAccessToken == null || oAuthAccessToken.get() == null) {
+        if (unwrapSupplier(oAuthAccessToken) == null) {
             if (!cr.getMetadata().containsKey(HttpHeaders.AUTHORIZATION)) {
                 cr.getMetadata().add(HttpHeaders.AUTHORIZATION, authentication);
             }
         } else {
             cr.getMetadata().remove(HttpHeaders.AUTHORIZATION);
-            cr.getHeaders().putSingle("Cookie", "authorization=" + oAuthAccessToken.get());
-            cr.getHeaders().putSingle("X-XSRF-TOKEN", xsrfToken.get());
+            cr.getHeaders().putSingle("Cookie", "authorization=" + unwrapSupplier(oAuthAccessToken));
+            cr.getHeaders().putSingle("X-XSRF-TOKEN", unwrapSupplier(xsrfToken));
         }
 
         return getNext().handle(cr);
+    }
+
+    /**
+     * This method allow keep backward compatibility.
+     * Now filter use Supplier and cannot guarantee if code provided by supplier will be run with our without
+     * Cumulocity context, that method must be resistant to fact if will be run with or without context.
+     *
+     * java-client can be use without context and this situation is correct and must be equal to situation when
+     * java-client is run with constant values for authentication and oauthAccessToken.
+     *
+     * Try catch block is for situation if someone define context aware oauthAccessToken supplier and execute java-client
+     * without Cumulocity context in such case filter will be assume that oauthAccessToken was not provided.
+     *
+     * In first implementation the value will be null if supplier will have any problem with returning value.
+     *
+     * @param stringSupplier - the string supplier
+     * @return nuwrapped string value
+     */
+    private String unwrapSupplier(Supplier<String> stringSupplier) {
+        try {
+            if (stringSupplier == null) {
+                return null;
+            }
+            return stringSupplier.get();
+        } catch (Exception e) {
+            log.debug("cannot unwrap oauth credentials because: {}", e.getMessage(), e);
+            return null;
+        }
     }
 
 }
