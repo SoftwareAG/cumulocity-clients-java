35019a223628f5a642d5e950aead92d24ff71447
Slawomir Chmiel <slawomir.chmiel@cumulocity.com>
2018-09-05 14:45:57 +0200

    MTM-22206 [B] Core node health endpoint with DOWN status is causing microservice to be DOWN

--- microservice/health-indicator/src/main/java/com/cumulocity/microservice/health/indicator/platform/PlatformHealthIndicator.java
+++ microservice/health-indicator/src/main/java/com/cumulocity/microservice/health/indicator/platform/PlatformHealthIndicator.java
@@ -3,25 +3,27 @@ package com.cumulocity.microservice.health.indicator.platform;
 import com.cumulocity.microservice.context.ContextService;
 import com.cumulocity.microservice.context.credentials.Credentials;
 import com.cumulocity.microservice.context.credentials.MicroserviceCredentials;
-import com.cumulocity.microservice.health.indicator.platform.PlatformHealthIndicatorProperties;
 import com.cumulocity.microservice.subscription.model.core.PlatformProperties;
+import com.cumulocity.model.authentication.CumulocityCredentials;
+import com.cumulocity.model.authentication.CumulocityLogin;
 import com.cumulocity.sdk.client.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.jersey.api.client.ClientResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.actuate.health.AbstractHealthIndicator;
 import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.Status;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import static javax.ws.rs.core.MediaType.*;
+import static java.lang.String.format;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
+import static org.apache.commons.httpclient.HttpStatus.SC_OK;
 
 @Slf4j
 @Component
@@ -36,33 +38,25 @@ public class PlatformHealthIndicator extends AbstractHealthIndicator {
     @Override
     @SuppressWarnings("unchecked")
     protected void doHealthCheck(Health.Builder builder) {
-        try (final Platform platform = platform().build()) {
-            final RestOperations rest = platform.rest();
-            try {
+        try {
+            final RestConnector rest = new RestConnector(platformParameters(), new ResponseParser(new JacksonResponseMapper()));
+            final ClientResponse response = rest.get(configuration.getPath(), APPLICATION_JSON_TYPE);
+            if (response.getStatus() == SC_OK) {
+                builder.up();
+            } else {
+                builder.unknown().withDetail("message", format("Platform respond with wrong status %s", response.getStatus()));
+            }
 
-                try {
-                    rest.get(configuration.getPath(), APPLICATION_JSON_TYPE, Map.class);
-                    builder.up();
-                } catch (SDKException ex) {
-                    throw ex;
-                }
-
-                if (configuration.getDetails().getEnabled()) {
-                    final Map<String, Object> details = rest.get(configuration.getDetails().getPath(), APPLICATION_JSON_TYPE, Map.class);
-                    if (details.containsKey("status")) {
-                        // tenant health endpoint contains detailed information about database connections
-                        assignDetails(builder, details);
-                    }
-                }
-
-            } catch (SDKException ex) {
-                if (ex.getHttpStatus() == HttpStatus.FORBIDDEN.value()) {
-                    builder.unknown().withDetail("message", ex.getMessage());
-                } else {
-                    builder.down(ex);
+            if (configuration.getDetails().getEnabled() && response.getStatus() == SC_OK) {
+                final Map<String, Object> details = rest.get(configuration.getDetails().getPath(), APPLICATION_JSON_TYPE, Map.class);
+                if (details.containsKey("status")) {
+                    // tenant health endpoint contains detailed information about database connections
+                    assignDetails(builder, details);
                 }
             }
             log.debug("health {}", builder.build());
+        } catch (Exception ex) {
+            builder.down(ex).withDetail("message", ex.getMessage());
         }
     }
 
@@ -75,27 +69,21 @@ public class PlatformHealthIndicator extends AbstractHealthIndicator {
         }
     }
 
-    private PlatformBuilder platform() {
-        final ObjectMapper mapper = new ObjectMapper();
+    private PlatformParameters platformParameters() {
         final Credentials context = getCredentials();
-        return PlatformBuilder.platform()
-                .withBaseUrl(properties.getUrl().get())
-                .withUsername(context.getUsername())
-                .withPassword(context.getPassword())
-                .withOAuthAccessToken(context.getOAuthAccessToken())
-                .withXsrfToken(context.getXsrfToken())
-                .withTenant(context.getTenant())
-                .withResponseMapper(new ResponseMapper() {
-                    @SneakyThrows
-                    public <T> T read(InputStream stream, Class<T> clazz) {
-                        return mapper.readValue(stream, clazz);
-                    }
 
-                    @SneakyThrows
-                    public String write(Object object) {
-                        return mapper.writeValueAsString(object);
-                    }
-                });
+        final CumulocityCredentials credentials = new CumulocityCredentials(
+                CumulocityLogin.fromLoginString(context.getTenant() + "/" + context.getUsername()),
+                context.getPassword(),
+                context.getOAuthAccessToken(),
+                context.getXsrfToken(),
+                context.getAppKey(),
+                null
+        );
+        final PlatformParameters params = new PlatformParameters(properties.getUrl().get(), credentials, new ClientConfiguration());
+        params.setForceInitialHost(properties.getForceInitialHost());
+        params.setTfaToken(context.getTfaToken());
+        return params;
     }
 
     private Credentials getCredentials() {
@@ -103,5 +91,23 @@ public class PlatformHealthIndicator extends AbstractHealthIndicator {
             return microservice.getContext();
         }
         return properties.getMicroserviceBoostrapUser();
+    }
+
+    private static final class JacksonResponseMapper implements ResponseMapper {
+        private final ObjectMapper mapper;
+
+        public JacksonResponseMapper() {
+            this.mapper = new ObjectMapper();
+        }
+
+        @SneakyThrows
+        public <T> T read(InputStream stream, Class<T> clazz) {
+            return mapper.readValue(stream, clazz);
+        }
+
+        @SneakyThrows
+        public String write(Object object) {
+            return mapper.writeValueAsString(object);
+        }
     }
 }
