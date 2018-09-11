247f11acc2d44d6db213bf59e98f4d214ceaee8e
Jaroslaw Herod <jaroslaw.herod@cumulocity.com>
2018-09-11 13:52:44 +0200

    MTM-22264 (Jenkins) - fix build

--- .jenkins/scripts/release.sh
+++ .jenkins/scripts/release.sh
@@ -3,4 +3,4 @@ set -e
 release_args="-Dmaven.javadoc.skip=true -Dskip.microservice.package=false -Dskip.agent.package.container=false -Dnexus.url=http://nexus:8081  -Darguments=-Dskip.microservice.package=false -Dskip.agent.package.rpm=false -Dskip.agent.package.container=false -Dnexus.url=http://nexus:8081"
 source ${BASH_SOURCE%/*}/common.sh
 call-mvn clean -T 4
-call-mvn release:prepare release:perform ${release_args} -DignoreSnapshots=true
+call-mvn release:prepare release:perform ${release_args} -DignoreSnapshots=true -DallowTimestampedSnapshots=true
