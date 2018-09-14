dbbf485597741b270513680fe21f2b32282c8628
Jaroslaw Herod <jaroslaw.herod@cumulocity.com>
2018-09-14 14:04:52 +0200

    MTM-22264 (Jenkinsfile) - add bitbucket build status

--- .jenkins/scripts/status.sh
+++ .jenkins/scripts/status.sh
@@ -1,6 +1,8 @@
 
 
 
-function update-status{
-    curl --user $BITBUCKET_USER:$BITBUCKET_PASSWORD https://bitbucket.org/rest/build-status/1.0/commits/${MERCURIAL_REVISION} --data "{\"state\":\"$1\", \"name\": \"$2\", \"key\": \"$3\", \"url\": \"\", \"description\": \"\" }"
+function update-status {
+    echo $(curl -s --user ${BITBUCKET_USER}:${BITBUCKET_PASSWORD} -H "Content-Type: application/json" -X POST https://bitbucket.org/rest/build-status/1.0/commits/${MERCURIAL_REVISION} --data "{\"state\":\"${1}\", \"name\": \"${2}\", \"key\": \"${3}\", \"url\": \"\", \"description\": \"\" }")
 }
+
+update-status $1 $2 $3 
