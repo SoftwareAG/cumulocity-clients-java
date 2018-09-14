9be045eaa6ad266efca1664a8e826ae4b455bb00
Jaroslaw Herod <jaroslaw.herod@cumulocity.com>
2018-09-14 14:56:26 +0200

    MTM-22264 (Jenkinsfile) - Bitbucket build staus update

--- .jenkins/scripts/status.sh
+++ .jenkins/scripts/status.sh
@@ -2,7 +2,12 @@
 
 
 function update-status {
-    echo $(curl -s --user ${BITBUCKET_USER}:${BITBUCKET_PASSWORD} -H "Content-Type: application/json" -X POST https://api.bitbucket.org/2.0/repositories/m2m/cumulocity-clients-java/commit/${MERCURIAL_REVISION}/statuses/build --data "{\"state\":\"${1}\", \"name\": \"${2}\", \"key\": \"${3}\", \"url\": \"http://localhost:8081/job/Cumulocity-JavaSDK/job/${BRANCH_NAME}/${BUILD_ID}/\", \"description\": \"\" }")
+    commitId=${MERCURIAL_REVISION}
+    status=${1}
+    name=${2}
+    key=$( echo $3 | cat -c -40 )
+    echo "Sending build status update for  commitId: ${commitId}, status: ${status}, name: ${name} , key: ${key}"
+    echo $(curl -s --user ${BITBUCKET_USER}:${BITBUCKET_PASSWORD} -H "Content-Type: application/json" -X POST https://api.bitbucket.org/2.0/repositories/m2m/cumulocity-clients-java/commit/${commitId}/statuses/build --data "{\"state\":\"${status}\", \"name\": \"${name}\", \"key\": \"${key}\", \"url\": \"http://localhost:8081/job/Cumulocity-JavaSDK/job/${BRANCH_NAME}/${BUILD_ID}/\", \"description\": \"\" }")
 }
 
 update-status $1 $2 $3 
