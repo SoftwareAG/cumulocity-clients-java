872c2d9e331b16ad4b6d00e6481806668432e4ad
Jaroslaw Herod <jaroslaw.herod@cumulocity.com>
2018-09-14 14:22:53 +0200

    MTM-22264 (Jenkinsfile) - add bitbucket build status

--- .jenkins/scripts/status.sh
+++ .jenkins/scripts/status.sh
@@ -2,7 +2,7 @@
 
 
 function update-status {
-    echo $(curl -s --user ${BITBUCKET_USER}:${BITBUCKET_PASSWORD} -H "Content-Type: application/json" -X POST https://api.bitbucket.org/2.0/repositories/m2m/cumulocity-clients-java/commit/${MERCURIAL_REVISION}/statuses/build --data "{\"state\":\"${1}\", \"name\": \"${2}\", \"key\": \"${3}\", \"url\": \"\", \"description\": \"\" }")
+    echo $(curl -s --user ${BITBUCKET_USER}:${BITBUCKET_PASSWORD} -H "Content-Type: application/json" -X POST https://api.bitbucket.org/2.0/repositories/m2m/cumulocity-clients-java/commit/${MERCURIAL_REVISION}/statuses/build --data "{\"state\":\"${1}\", \"name\": \"${2}\", \"key\": \"${3}\", \"url\": \"http://localhost:8081/job/Cumulocity-JavaSDK/job/${BRANCH_NAME}/${BUILD_ID}/\", \"description\": \"\" }")
 }
 
 update-status $1 $2 $3 
