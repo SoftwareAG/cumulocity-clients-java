cd3c0442154c56f16f7abf5ef639a1029444e74f
Kamil Sobala <kamil.sobala@cumulocity.com>
2019-02-25 10:28:43 +0100

    MTM-24478: Platform memory and thread leak in microservice bugfix

--- java-client/src/main/java/com/cumulocity/sdk/client/PlatformParameters.java
+++ java-client/src/main/java/com/cumulocity/sdk/client/PlatformParameters.java
@@ -112,7 +112,6 @@ public class PlatformParameters {
     public synchronized RestConnector createRestConnector() {
         if (restConnector == null) {
             restConnector = new RestConnector(this, new ResponseParser(responseMapper));
-            startBufferProcessing();
         }
         return restConnector;
     }
@@ -221,7 +220,10 @@ public class PlatformParameters {
         return cumulocityCredentials == null ? null : cumulocityCredentials.getRequestOrigin();
     }
 
-    BufferRequestService getBufferRequestService() {
+    BufferRequestService createBufferRequestService() {
+        if (bufferRequestService == null) {
+            startBufferProcessing();
+        }
         return bufferRequestService;
     }
 
