4a099e0611b7f067433e5238efc90e006fe2b795
Jaroslaw Herod <jaroslaw.herod@cumulocity.com>
2019-02-28 13:10:22 +0100

    #nossie - Improve performance of microservice authentication by adding cache

--- microservice/security/src/main/java/com/cumulocity/microservice/security/service/SecurityUserDetailsService.java
+++ microservice/security/src/main/java/com/cumulocity/microservice/security/service/SecurityUserDetailsService.java
@@ -1,6 +1,9 @@
 package com.cumulocity.microservice.security.service;
 
+import com.cumulocity.model.authentication.CumulocityCredentials;
 import com.cumulocity.sdk.client.PlatformParameters;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -8,12 +11,20 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Repository;
 
 import java.beans.ConstructorProperties;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 @Repository
 public class SecurityUserDetailsService implements UserDetailsService {
 
     private final PlatformParameters platformParameters;
     private final RoleService roleService;
+    private final Cache<CumulocityCredentials, UserDetails> userDetails = CacheBuilder.newBuilder()
+            .concurrencyLevel(Runtime.getRuntime().availableProcessors() * 2)
+            .expireAfterWrite(10, SECONDS)
+            .build();
 
     @Autowired
     @ConstructorProperties({"userPlatform", "roleService"})
@@ -24,8 +35,18 @@ public class SecurityUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
-        return SecurityUserDetails.activeUser(roleService.getUserRoles())
-                .credentials(platformParameters.getCumulocityCredentials())
-                .build();
+        final CumulocityCredentials credentials = platformParameters.getCumulocityCredentials();
+        try {
+            return userDetails.get(credentials, new Callable<UserDetails>() {
+                @Override
+                public UserDetails call() {
+                    return SecurityUserDetails.activeUser(roleService.getUserRoles())
+                            .credentials(credentials)
+                            .build();
+                }
+            });
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
