6954c3171567a6f6eaa4e8dc10e06c9e46aab7e3
Dariusz Dudziński <dariusz.dudzinski@softwareag.com>
2019-05-22 11:28:33 +0200

    Graft release process

--- .jenkins/scripts/compile.sh
+++ .jenkins/scripts/compile.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 set -e
 source ${BASH_SOURCE%/*}/common.sh
-./mvnw install -DskipTests -s $MVN_SETTINGS -U
-cd microservice
-../mvnw install -DskipTests -s $MVN_SETTINGS -U
-cd -
+call-mvn install -DskipTests -s $MVN_SETTINGS -U
