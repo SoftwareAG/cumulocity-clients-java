04f1464784446e72c087c9b0aa912d4e486d4d6d
Łukasz Prażmo <lukasz.prazmo@cumulocity.com>
2019-07-09 12:17:15 +0200

    [MTM-26178] Improvement of tests

--- java-client-services/src/main/test/java/com.cumulocity.sdk.services.client/ServicePlatformImplTest.java
+++ java-client-services/src/main/test/java/com.cumulocity.sdk.services.client/ServicePlatformImplTest.java
@@ -3,25 +3,34 @@ package com.cumulocity.sdk.services.client;
 import com.cumulocity.model.authentication.CumulocityBasicCredentials;
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
 
 public class ServicePlatformImplTest {
 
     @Test
-    public void testCreateObjectWhenHostContainsUnderscoreCharacter() {
+    public void shouldCreateObjectWhenAddressContainsUnderscoreCharacter() {
         String host = "http://test_tenant.test_environment.c8y.io";
 
-        ServicesPlatform platform = new ServicesPlatformImpl(
+        ServicesPlatform platform = sampleServicesPlatform(host);
+    }
+
+    @Test
+    public void shouldGetApisForGivenPlatformAddress() {
+        String host = "http://test-tenant.test-environment.c8y.io";
+
+        ServicesPlatform platform = sampleServicesPlatform(host);
+
+        platform.getSmsMessagingApi();
+        platform.getEmailApi();
+    }
+
+    private ServicesPlatform sampleServicesPlatform(String host) {
+        return new ServicesPlatformImpl(
                 host,
                 CumulocityBasicCredentials.builder()
                         .username("admin")
-                        .tenantId("test_tenant")
                         .password("password")
+                        .tenantId("test")
                         .build()
         );
-
-        assertNotNull(platform);
-        assertNotNull(platform.getSmsMessagingApi());
-        assertNotNull(platform.getEmailApi());
     }
 }
