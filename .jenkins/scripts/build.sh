812be59aaa5de30d60d24a57d6f7f6890345212a
kczu <kczu@kczu-HP-EliteBook-850-G5>
2019-07-25 13:20:49 +0200

    MTM-27485 add maven offline to clients java v30

--- .jenkins/scripts/build.sh
+++ .jenkins/scripts/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set +eu
-[[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
 set -e
-./mvnw -o install -Dmaven.repo.local=.m2/repository
+source ${BASH_SOURCE%/*}/common.sh
+./mvnw -o install
--- .jenkins/scripts/compile.sh
+++ .jenkins/scripts/compile.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-set +eu
-[[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
 set -e
-./mvnw install -DskipTests -s $MVN_SETTINGS -U -Dmaven.repo.local=.m2/repository
-./mvnw de.qaware.maven:go-offline-maven-plugin:resolve-dependencies -Dmaven.repo.local=.m2/repository
+source ${BASH_SOURCE%/*}/common.sh
+./mvnw install -DskipTests -s $MVN_SETTINGS -U
+./mvnw de.qaware.maven:go-offline-maven-plugin:resolve-dependencies
