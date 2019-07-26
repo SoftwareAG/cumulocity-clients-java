10f383e8a7bc51c8883e883c15517334efd1eda7
kczu <kczu@kczu-HP-EliteBook-850-G5>
2019-07-26 10:12:33 +0200

    MTM-27485 add maven offline to clients java v72

--- .jenkins/scripts/build.sh
+++ .jenkins/scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 source ${BASH_SOURCE%/*}/common.sh
-./mvnw -o clean install -Dskip.microservice.package=false -Dskip.agent.package.container=false -Pci
+./mvnw -o install -Pci
 cd microservice
-../mvnw -o clean install -Dskip.microservice.package=false -Dskip.agent.package.container=false -Pci
+../mvnw -o install -Pci
 cd -
