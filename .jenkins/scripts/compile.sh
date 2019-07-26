ada1ca8c474bd6da34c6513f9895b06c09b0b27c
kczu <kczu@kczu-HP-EliteBook-850-G5>
2019-07-26 15:54:40 +0200

    MTM-27485 add maven offline to clients java v98

--- .jenkins/scripts/compile.sh
+++ .jenkins/scripts/compile.sh
@@ -2,4 +2,5 @@
 set -e
 source ${BASH_SOURCE%/*}/common.sh
 ./mvnw clean install -DskipTests -s $MVN_SETTINGS -U
+./mvnw dependency:go-offline
 ./mvnw de.qaware.maven:go-offline-maven-plugin:resolve-dependencies
