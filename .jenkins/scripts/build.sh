be5660f317a136ff8869d0dfd010f56e412dab05
kczu <kczu@kczu-HP-EliteBook-850-G5>
2019-07-26 11:31:48 +0200

    MTM-27485 add maven offline to clients java v85

--- .jenkins/scripts/build.sh
+++ .jenkins/scripts/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 source ${BASH_SOURCE%/*}/common.sh
+./mvnw -o install -Pci
 cd microservice
 ../mvnw -o install -Pci
 cd -
--- .jenkins/scripts/compile.sh
+++ .jenkins/scripts/compile.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 source ${BASH_SOURCE%/*}/common.sh
+./mvnw install -DskipTests -s $MVN_SETTINGS -U
+./mvnw de.qaware.maven:go-offline-maven-plugin:resolve-dependencies
 cd microservice
 ../mvnw install -DskipTests -s $MVN_SETTINGS -U
 ../mvnw de.qaware.maven:go-offline-maven-plugin:resolve-dependencies
