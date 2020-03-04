e8c97b9d077749608d60067415e08800da3f050d
Pawel Kowalczyk <Pawel.Kowalczyk@softwareag.com>
2020-03-04 07:49:44 +0100

    Set upstream in release

--- .jenkins/scripts/release.sh
+++ .jenkins/scripts/release.sh
@@ -28,10 +28,15 @@ repository_url=https://${BITBUCKET_USER}:${BITBUCKET_PASSWORD}@bitbucket.org/m2m
 
 echo checkout to new branch
 git checkout ${branch_name}
+git push --set-upstream origin ${branch_name}
+git branch --set-upstream-to=origin/${branch_name} ${branch_name}
 echo pull changes
 git pull
+
 cd cumulocity-sdk
 git checkout ${branch_name}
+git push --set-upstream origin ${branch_name}
+git branch --set-upstream-to=origin/${branch_name} ${branch_name}
 git pull
 cd -
 
