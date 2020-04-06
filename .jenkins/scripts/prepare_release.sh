c9d6851b73540b32d91f01c4a647d6384db7e445
Pawel Kowalczyk <Pawel.Kowalczyk@softwareag.com>
2020-04-06 10:31:54 +0200

    noissue: Fix sdk' update dependencies for release, jenkins statuses

--- .jenkins/scripts/prepare_release.sh
+++ .jenkins/scripts/prepare_release.sh
@@ -5,10 +5,10 @@ branch_name=release/r$release_version
 echo "equivalent of git flow release start $release_version"
 git branch $branch_name
 git checkout $branch_name
-git commit -am "flow: Created branch release/${release_version}" --allow-empty
+git commit -am "flow: Created branch ${branch_name}" --allow-empty
 
 cd cumulocity-sdk
 git branch $branch_name
 git checkout $branch_name
-git commit -am "flow: Created branch release/${release_version}" --allow-empty
+git commit -am "flow: Created branch ${branch_name}" --allow-empty
 cd -
