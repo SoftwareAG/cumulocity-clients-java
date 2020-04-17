bd16f533af75752c608cddd01e0a60b547d62299
Pawel Kowalczyk <Pawel.Kowalczyk@softwareag.com>
2020-04-17 12:18:21 +0200

    MTM-30761: Added models for c8y_SupportedLogs and c8y_SupportedConfigurations

---
+++ device-capability-model/src/main/java/c8y/SupportedConfigurations.java
@@ -0,0 +1,7 @@
+package c8y;
+
+import java.util.ArrayList;
+
+public class SupportedConfigurations extends ArrayList<String> {
+	private static final long serialVersionUID = 6871829835760768955L;
+}
