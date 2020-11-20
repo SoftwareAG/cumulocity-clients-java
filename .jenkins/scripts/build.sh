2e1fae3cc2790a911076b8f4e85c4c06fc7fa42f
Jaroslaw Herod <jaroslaw.herod@softwareag.com>
2020-11-20 11:31:46 +0100

    MTM-35567 - add support for kubernetes agents

--- .jenkins/scripts/build.sh
+++ .jenkins/scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 source ${BASH_SOURCE%/*}/common.sh
-./mvnw install -Pci,javadoc
+./mvnw install -Pci,javadoc -s $MVN_SETTINGS -U
 cd microservice
-../mvnw install javadoc:aggregate-jar -Pci
+../mvnw install javadoc:aggregate-jar -Pci -s $MVN_SETTINGS -U
 cd -
