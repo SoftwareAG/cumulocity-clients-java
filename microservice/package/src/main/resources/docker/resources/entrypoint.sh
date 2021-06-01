c5bc52421c9a03c4b77173ce846b693b67d80feb
Kamil Sobala <kamil.sobala@cumulocity.com>
2021-06-01 08:59:25 +0200

    [MTM-34361]: Use G1 Garbage Collector in microservices

--- microservice/package/src/main/resources/docker/resources/entrypoint.sh
+++ microservice/package/src/main/resources/docker/resources/entrypoint.sh
@@ -18,24 +18,18 @@ if [ -n "$MEMORY_LIMIT" ];
   fi
 
   jvm_heap="@package.jvm-heap@"
-  jvm_perm="@package.jvm-perm@"
+  jvm_perm="@package.jvm-meta@"
   jvm_variable_heap="-Xmx${heap}m"
-  case $JAVA_VERSION in
-   7*)
-    echo "Using JDK7 memory settings"
-    jvm_variable_perm="-XX:MaxPermSize=${perm}m"
-   ;;
-   *)
-    echo "Using JDK8+ memory settings"
-    jvm_variable_perm="-XX:MaxMetaspaceSize=${perm}m"
-   ;;
-  esac
+
+  echo "Using JDK8+ memory settings"
+  jvm_variable_perm="-XX:MaxMetaspaceSize=${perm}m"
+
   export JAVA_MEM="${jvm_heap:-`echo $jvm_variable_heap`} ${jvm_perm:-`echo $jvm_variable_perm`}"
   echo "Java Memory Settings: $JAVA_MEM, memory limit: $MEMORY_LIMIT"
 fi
 
 jvm_gc=${JAVA_GC:-"@package.jvm-gc@"}
-jvm_mem=${JAVA_MEM:-"@package.jvm-heap@ @package.jvm-perm@"}
+jvm_mem=${JAVA_MEM:-"@package.jvm-heap@ @package.jvm-meta@"}
 jvm_opts=${JAVA_OPTS:-"-server -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/log/@package.directory@/heap-dump-%p.hprof"}
 arguments=${ARGUMENTS:-"@package.arguments@ --package.name=@package.name@ --package.directory=@package.directory@"}
 
