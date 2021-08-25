0a3d0ac42d2033b742d424c25f3056963944fbbe
Kamil Sobala <kamil.sobala@cumulocity.com>
2021-08-25 07:51:29 +0200

    [MTM-39495]: Migrate cumulocity-clients-java to github <githubdev>

--- .jenkins/scripts/finish_release.sh
+++ .jenkins/scripts/finish_release.sh
@@ -4,13 +4,13 @@ release_version=$1
 
 function git_finish_release {
   repository_url=$1
-  git pull $repository_url develop
+  git pull $repository_url githubdev
   git commit -am "flow: Closed <release> ${release_version}" --allow-empty
-  git checkout develop
+  git checkout githubdev
   git merge -s recursive -Xtheirs release/r${release_version}
-  git commit -am "flow: Merged <release> r${release_version} to <develop> (develop)."  --allow-empty
+  git commit -am "flow: Merged <release> r${release_version} to <githubdev> (githubdev)."  --allow-empty
   git push --follow-tags $repository_url release/r${release_version}
-  git push --follow-tags $repository_url develop
+  git push --follow-tags $repository_url githubdev
 }
 
 echo "equivalent of git flow release finish r${release_version}"
--- .jenkins/scripts/update_develop.sh
+++ .jenkins/scripts/update_develop.sh
@@ -3,21 +3,21 @@ set -e
 hotfix_version=$1
 development_version=$2
 
-git checkout develop
+git checkout githubdev
 cd cumulocity-sdk
-git checkout develop
+git checkout githubdev
 cd -
 
 find . -name 'pom.xml' | xargs sed -i "s/<version>${hotfix_version}<\\/version>/<version>${development_version}<\\/version>/g"
 .jenkins/scripts/update_dependencies.sh ${development_version}
 
 git commit -am 'Update dependencies to next SNAPSHOT version' --allow-empty
-git push --follow-tags ${REPOSITORY_CLIENTS_JAVA} develop
+git push --follow-tags ${REPOSITORY_CLIENTS_JAVA} githubdev
 
 cd cumulocity-sdk
 
 git commit -am 'Update to dependencies next SNAPSHOT version' --allow-empty
-git push --follow-tags ${REPOSITORY_SDK} develop
+git push --follow-tags ${REPOSITORY_SDK} githubdev
 
 cd -
 
