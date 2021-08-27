628388234c9df01b83f60bfe11120d639e09d370
Kamil Sobala <kamil.sobala@cumulocity.com>
2021-08-27 08:30:40 +0000

    [MTM-39495]: Migrate cumulocity-clients-java to github

--- .jenkins/scripts/update_develop.sh
+++ .jenkins/scripts/update_develop.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-
 source ${BASH_SOURCE%/*}/update_dependencies.sh
+set -e
 
 hotfix_version=$1
 development_version=$2
@@ -14,12 +14,12 @@ find . -name 'pom.xml' | xargs sed -i "s/<version>${hotfix_version}<\\/version>/
 # update-dependencies ${development_version}
 
 git commit -am 'Update dependencies to next SNAPSHOT version' --allow-empty
-git push --follow-tags https://${BITBUCKET_USER}:${BITBUCKET_PASSWORD}@bitbucket.org/m2m/cumulocity-clients-java develop
+git push --follow-tags ${REPOSITORY_CLIENTS_JAVA} develop
 
 cd cumulocity-sdk
 
 git commit -am 'Update to dependencies next SNAPSHOT version' --allow-empty
-git push --follow-tags https://${BITBUCKET_USER}:${BITBUCKET_PASSWORD}@bitbucket.org/m2m/cumulocity-sdk develop
+git push --follow-tags ${REPOSITORY_SDK} develop
 
 cd -
 
