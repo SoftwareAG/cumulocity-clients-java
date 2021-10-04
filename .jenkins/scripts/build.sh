9fc90acb016eb4f1cc5a7219ed3bc7c2841e1330
Robert Squire <rsq@softwareag.com>
2021-10-04 15:01:12 +0100

    Fix merge issue

--- .jenkins/scripts/build.sh
+++ .jenkins/scripts/build.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 set -e
-# source ${BASH_SOURCE%/*}/common.sh
+source ${BASH_SOURCE%/*}/common.sh
 
-./mvnw install -Pci,javadoc -s $MVN_SETTINGS -U $1
+./mvnw install -Pci,javadoc -s $MVN_SETTINGS -U
 cd microservice
-../mvnw install javadoc:aggregate-jar -Pci -s $MVN_SETTINGS -U $1
+../mvnw install javadoc:aggregate-jar -Pci -s $MVN_SETTINGS -U
 cd -
 cd lpwan-backend
-<<<<<<< HEAD
-../mvnw install javadoc:aggregate-jar -Pci -s $MVN_SETTINGS -U $1
-cd -
-=======
 ../mvnw install javadoc:aggregate-jar -Pci -s $MVN_SETTINGS -U
 cd -
 
->>>>>>> bitbucket/develop
