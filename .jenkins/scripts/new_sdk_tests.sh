91f55544087ef4cdf5405b615a37901e5be2a53c
Łukasz Pospiech <lukasz.pospiech@softwareag.com>
2021-11-04 13:19:24 +0100

    IOT-12358 add possibility to specify admin username for management tenant in functional tests

--- .jenkins/scripts/new_sdk_tests.sh
+++ .jenkins/scripts/new_sdk_tests.sh
@@ -3,8 +3,13 @@
 DEPLOYMENT_ENVIRONMENT=$1
 MANAGE_PASS=$2
 
+if [[ -z "$3"]]; then
+  MANAGE_USER=admin
+else
+  MANAGE_USER="$3"
+fi
 
-./mvnw -s $MVN_SETTINGS clean install -Pintegration -Dcumulocity.host=http://${DEPLOYMENT_ENVIRONMENT} -Dcumulocity.management.password=${MANAGE_PASS}
+./mvnw -s $MVN_SETTINGS clean install -Pintegration -Dcumulocity.host=http://${DEPLOYMENT_ENVIRONMENT} -Dcumulocity.management.password=${MANAGE_PASS} -Dcumulocity.management.username=${MANAGE_USER}
 
 
 
