672dbd202c2c2d5362238134c97cbc454d734a0c
atul <atulkumar.panda@cumulocity.com>
2021-11-29 10:27:38 +0530

    Unit test for PayloadDecoderService

--- lpwan-backend/src/test/java/com/cumulocity/lpwan/payload/service/PayloadDecoderServiceTest.java
+++ lpwan-backend/src/test/java/com/cumulocity/lpwan/payload/service/PayloadDecoderServiceTest.java
@@ -1,29 +1,50 @@
 package com.cumulocity.lpwan.payload.service;
 
+import com.cumulocity.lpwan.codec.model.DecoderOutput;
+import com.cumulocity.lpwan.codec.model.LpwanCodecDetails;
+import com.cumulocity.lpwan.devicetype.model.DeviceType;
 import com.cumulocity.lpwan.devicetype.model.UplinkConfiguration;
 import com.cumulocity.lpwan.payload.exception.PayloadDecodingFailedException;
-import com.cumulocity.lpwan.payload.uplink.model.MessageIdConfiguration;
 import com.cumulocity.lpwan.payload.uplink.model.UplinkMessage;
-import org.junit.jupiter.api.Test;
+import com.cumulocity.microservice.context.ContextService;
+import com.cumulocity.microservice.context.credentials.MicroserviceCredentials;
+import com.cumulocity.microservice.subscription.service.MicroserviceSubscriptionsService;
+import com.cumulocity.model.idtype.GId;
+import com.cumulocity.rest.representation.inventory.ManagedObjects;
+import org.joda.time.DateTime;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class PayloadDecoderServiceTest {
 
-    private PayloadDecoderService<UplinkMessage> payloadDecoderService = new PayloadDecoderServiceImpl(null);
+    @Mock
+    private MicroserviceSubscriptionsService subscriptionsService;
 
-    private class PayloadDecoderServiceImpl extends PayloadDecoderService<UplinkMessage> {
+    @Mock
+    private ContextService<MicroserviceCredentials> contextService;
 
-        public PayloadDecoderServiceImpl(PayloadMappingService payloadMappingService) {
-            super(payloadMappingService, new MessageIdReader<UplinkMessage>() {
-                @Override
-                public Integer read(UplinkMessage uplinkMessage, MessageIdConfiguration messageIdConfiguration) {
-                    return null;
-                }
-            });
-        }
-    }
+    @Mock
+    DecoderOutput decoderOutput;
+
+    ArgumentCaptor<Runnable> taskCaptor = ArgumentCaptor.forClass(Runnable.class);
+
+    @InjectMocks
+    PayloadDecoderService<UplinkMessage> payloadDecoderService = new PayloadDecoderService<>(null, null);
+
 
     @Test
     public void decodeLittleEndianHex() {
@@ -92,5 +113,30 @@ public class PayloadDecoderServiceTest {
         } catch (PayloadDecodingFailedException e) {
             fail();
         }
+    }
+
+    @Test
+    public void shouldTestPayloadDecodeAndMap() throws PayloadDecodingFailedException {
+        UplinkMessage uplinkMessage = Mockito.mock(UplinkMessage.class, Mockito.CALLS_REAL_METHODS);
+        when(uplinkMessage.getPayloadHex()).thenReturn("ABCDEF1234567");
+        when(uplinkMessage.getExternalId()).thenReturn("deviceExternalId");
+        when(uplinkMessage.getFport()).thenReturn(1);
+        when(uplinkMessage.getDateTime()).thenReturn(new DateTime("2020-11-28T10:11:12.123"));
+
+        LpwanCodecDetails lpwanCodecDetails = new LpwanCodecDetails("Manufacturer_X", "Model_X", "lpwanContextPath");
+
+        DeviceType deviceType  = new DeviceType();
+        deviceType.setLpwanCodecDetails(lpwanCodecDetails);
+        deviceType.setFieldbusType("LORA");
+
+        MicroserviceCredentials credentials = new MicroserviceCredentials("tenant", "username", "password", null, null, null, "appKey");
+        when(contextService.getContext()).thenReturn(credentials);
+        when(subscriptionsService.getTenant()).thenReturn("tenant");
+        when(subscriptionsService.getCredentials(eq("tenant"))).thenReturn(Optional.of(credentials));
+
+        payloadDecoderService.decodeAndMap(uplinkMessage, ManagedObjects.asManagedObject(GId.asGId("12345")), deviceType);
+
+        verify(contextService).runWithinContext(eq(credentials), taskCaptor.capture());
+        taskCaptor.getValue().run();
     }
 }
