a3508894c03ead7985a213f53d212c8d2d0f86a9
Łukasz Pospiech <lukasz.pospiech@softwareag.com>
2021-12-10 13:26:51 +0100

    print additional args

--- .jenkins/scripts/new_sdk_tests.sh
+++ .jenkins/scripts/new_sdk_tests.sh
@@ -4,13 +4,15 @@ DEPLOYMENT_ENVIRONMENT=$1
 MANAGE_PASS=$2
 ADDITIONAL_BUILD_ARGS="${BUILD_ARGS:-''}"
 
+echo "additional args: $ADDITIONAL_BUILD_ARGS"
+
 if [[ -z "$3" ]]; then
   MANAGE_USER=admin
 else
   MANAGE_USER="$3"
 fi
 
-./mvnw -s $MVN_SETTINGS clean install -Pintegration -Dcumulocity.host=http://${DEPLOYMENT_ENVIRONMENT} -Dcumulocity.management.password=${MANAGE_PASS} -Dcumulocity.management.username=${MANAGE_USER} $ADDITIONAL_BUILD_ARGS
+./mvnw -s $MVN_SETTINGS clean install $ADDITIONAL_BUILD_ARGS -Pintegration -Dcumulocity.host=http://${DEPLOYMENT_ENVIRONMENT} -Dcumulocity.management.password=${MANAGE_PASS} -Dcumulocity.management.username=${MANAGE_USER}
 
 
 
