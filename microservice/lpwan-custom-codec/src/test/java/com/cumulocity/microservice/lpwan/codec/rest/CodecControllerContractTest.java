7db8e8aad97c40a56b436f5885d013d818881873
Ashwin Subramaniam Hariharan <ashh@softwareag.com>
2022-03-25 10:19:25 +0530

    Code Review Changes

--- microservice/lpwan-custom-codec/src/test/java/com/cumulocity/microservice/lpwan/codec/rest/CodecControllerContractTest.java
+++ microservice/lpwan-custom-codec/src/test/java/com/cumulocity/microservice/lpwan/codec/rest/CodecControllerContractTest.java
@@ -1,5 +1,8 @@
-package com.cumulocity.microservice.lpwan.codec.sample;
+package com.cumulocity.microservice.lpwan.codec.rest;
 
+import com.cumulocity.microservice.lpwan.codec.sample.Application;
+import com.cumulocity.microservice.lpwan.codec.sample.SwaggerConfiguration;
+import com.cumulocity.microservice.lpwan.codec.sample.TestBeanConfiguration;
 import io.github.robwin.swagger.test.SwaggerAssertions;
 import io.swagger.models.Swagger;
 import io.swagger.parser.SwaggerParser;
@@ -17,6 +20,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
@@ -24,12 +28,13 @@ import java.nio.charset.StandardCharsets;
 // TestBeanConfiguration is required, as the DecoderService and EncoderService beans are required to startup the application and are required by the CodecController.
 @SpringBootTest(classes = {Application.class, TestBeanConfiguration.class, SwaggerConfiguration.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @TestPropertySource(properties = "server.port=9192")
+@TestPropertySource(properties = "spring.main.allow-bean-definition-overriding=true")
 public class CodecControllerContractTest {
     private final String applicationUrl = "http://localhost:9192";
 
     @Test
-    public void validateThatImplementationSatisfiesConsumerSpecification() {
-        File designFirstSwagger = new File(CodecControllerContractTest.class.getResource("/goldenContract.yaml").getFile());
+    public void validateThatImplementationSatisfiesConsumerSpecification() throws URISyntaxException {
+        File designFirstSwagger = new File("src/test/java/com/cumulocity/microservice/lpwan/codec/rest/goldenContract.yaml");
         SwaggerAssertions.assertThat(applicationUrl + "/v2/api-docs")
                 .isEqualTo(designFirstSwagger.getAbsolutePath());
     }
