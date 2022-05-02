28dc89a5a293d3cef9d35035917d2ff60de7b152
mbak <michal.bak@cumulocity.com>
2022-05-02 13:59:30 +0200

    [MTM-44368] MINOR update after CR

--- microservice/security/src/test/java/com/cumulocity/microservice/security/filter/util/HttpRequestUtilsTest.java
+++ microservice/security/src/test/java/com/cumulocity/microservice/security/filter/util/HttpRequestUtilsTest.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Test;
 class HttpRequestUtilsTest {
 
     //Base 64 credentials coded: "management/rtanaka@hks-power.co.jp:(Def_Leppard)1"
-    private final static String asciiCharactersCredentials = "Basic bWFuYWdlbWVudC9ydGFuYWthQGhrcy1wb3dlci5jby5qcDooRGVmX0xlcHBhcmQpMQ==";
+    private static final  String asciiCharactersCredentials = "Basic bWFuYWdlbWVudC9ydGFuYWthQGhrcy1wb3dlci5jby5qcDooRGVmX0xlcHBhcmQpMQ==";
 
     //Base 64 credentials coded: "management/田中隆太:(Def_Leppard)1"
-    private final static String notAsciiCharactersCredentials = "Basic bWFuYWdlbWVudC/nlLDkuK3pmoblpKo6KERlZl9MZXBwYXJkKTE=";
+    private static final  String notAsciiCharactersCredentials = "Basic bWFuYWdlbWVudC/nlLDkuK3pmoblpKo6KERlZl9MZXBwYXJkKTE=";
 
     @Test
     void shouldDecodeAsciiCharactersCredentials() {
