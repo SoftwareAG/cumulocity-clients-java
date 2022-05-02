8e8c62d77f3e2ab93292c34b971975a6a7118820
mbak <michal.bak@cumulocity.com>
2022-05-02 14:05:55 +0200

    [MTM-44368] MINOR rename

--- microservice/security/src/test/java/com/cumulocity/microservice/security/filter/util/HttpRequestUtilsTest.java
+++ microservice/security/src/test/java/com/cumulocity/microservice/security/filter/util/HttpRequestUtilsTest.java
@@ -6,17 +6,17 @@ import org.junit.jupiter.api.Test;
 class HttpRequestUtilsTest {
 
     //Base 64 credentials coded: "management/rtanaka@hks-power.co.jp:(Def_Leppard)1"
-    private static final  String asciiCharactersCredentials = "Basic bWFuYWdlbWVudC9ydGFuYWthQGhrcy1wb3dlci5jby5qcDooRGVmX0xlcHBhcmQpMQ==";
+    private static final  String ASCII_CHARACTERS_CREDENTIALS = "Basic bWFuYWdlbWVudC9ydGFuYWthQGhrcy1wb3dlci5jby5qcDooRGVmX0xlcHBhcmQpMQ==";
 
     //Base 64 credentials coded: "management/田中隆太:(Def_Leppard)1"
-    private static final  String notAsciiCharactersCredentials = "Basic bWFuYWdlbWVudC/nlLDkuK3pmoblpKo6KERlZl9MZXBwYXJkKTE=";
+    private static final  String NOT_ASCII_CHARACTERS_CREDENTIALS = "Basic bWFuYWdlbWVudC/nlLDkuK3pmoblpKo6KERlZl9MZXBwYXJkKTE=";
 
     @Test
     void shouldDecodeAsciiCharactersCredentials() {
         // Given
         final String[] expectedArguments = {"management/rtanaka@hks-power.co.jp", "(Def_Leppard)1"};
         // When
-        final String[] decoded = HttpRequestUtils.decode(asciiCharactersCredentials);
+        final String[] decoded = HttpRequestUtils.decode(ASCII_CHARACTERS_CREDENTIALS);
         // Then
         Assertions.assertArrayEquals(expectedArguments, decoded);
     }
@@ -26,7 +26,7 @@ class HttpRequestUtilsTest {
         // Given
         final String[] expectedArguments = {"management/田中隆太", "(Def_Leppard)1"};
         // When
-        final String[] decoded = HttpRequestUtils.decode(notAsciiCharactersCredentials);
+        final String[] decoded = HttpRequestUtils.decode(NOT_ASCII_CHARACTERS_CREDENTIALS);
         // Then
         Assertions.assertArrayEquals(expectedArguments, decoded);
 
