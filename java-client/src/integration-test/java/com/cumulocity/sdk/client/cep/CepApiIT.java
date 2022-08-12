435dbad8bb42802726eb71af731efe3d8ffda1b5
Michal-Bak <89777350+Michal-Bak@users.noreply.github.com>
2022-08-12 13:19:35 +0200

    Bugfix/mtm 46135/esper it in staging randomly failing wait till esper will be subscribed graft 10.14.x (#184)
    * [MTM-46135] Cep esper integration tests randomly failing on staging e… (#147)
    
    * [MTM-46135] Cep esper integration tests randomly failing on staging environment: wait till cep will be successfully subscribed
    
    * [MTM-46135] Correct condition await.until
    
    * [MTM-46135] Wait till cep will be successfully started in target test tenant
    
    Co-authored-by: mbak <michal.bak@cumulocity.com>
    
    * [MTM-46135] Revert unnecessary wait for cep successfully subscribe in management context
    
    Co-authored-by: mbak <michal.bak@cumulocity.com>

--- java-client/src/integration-test/java/com/cumulocity/sdk/client/cep/CepApiIT.java
+++ java-client/src/integration-test/java/com/cumulocity/sdk/client/cep/CepApiIT.java
@@ -1,18 +1,23 @@
 package com.cumulocity.sdk.client.cep;
 
 import com.cumulocity.rest.representation.cep.CepModuleRepresentation;
+import com.cumulocity.sdk.client.SDKException;
 import com.cumulocity.sdk.client.common.JavaSdkITBase;
 import com.google.common.base.Charsets;
 import com.google.common.collect.Lists;
 import com.google.common.io.Resources;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URL;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+import static org.awaitility.Durations.TEN_SECONDS;
+import static org.awaitility.Durations.TWO_MINUTES;
 
 public class CepApiIT extends JavaSdkITBase {
 
@@ -21,6 +26,14 @@ public class CepApiIT extends JavaSdkITBase {
     @BeforeAll
     public static void makeSureCepEsperIsSubscribed() throws IOException {
         applicationApi.unsubscribeApamaAndSubscribeEsper();
+    }
+
+    @BeforeEach
+    public void waitUntilCepEsperWillBeReady() {
+        await().timeout(TWO_MINUTES)
+                .pollInterval(TEN_SECONDS)
+                .ignoreException(SDKException.class)
+                .until(() -> cepApi.getModules().get() != null);
     }
 
     @AfterEach
