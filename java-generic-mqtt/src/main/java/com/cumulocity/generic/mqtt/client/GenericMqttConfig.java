d25447a74540fac4ae5ad199dd796688ed4c22fe
Rohit Joshi <rohj@softwareag.com>
2023-04-12 19:56:48 +0530

    MTM-51435 doc

--- java-generic-mqtt/src/main/java/com/cumulocity/generic/mqtt/client/GenericMqttConfig.java
+++ java-generic-mqtt/src/main/java/com/cumulocity/generic/mqtt/client/GenericMqttConfig.java
@@ -3,6 +3,9 @@ package com.cumulocity.generic.mqtt.client;
 import com.cumulocity.generic.mqtt.client.websocket.GenericMqttWebSocketConfig;
 import com.cumulocity.generic.mqtt.client.websocket.GenericMqttWebSocketConfig.GenericMqttWebSocketConfigBuilder;
 
+/**
+ * {@link GenericMqttConfig} provides interface for configuring instances of {@link GenericMqttPublisher} and {@link GenericMqttSubscriber}
+ */
 public interface GenericMqttConfig {
 
     static GenericMqttWebSocketConfigBuilder webSocket() {
