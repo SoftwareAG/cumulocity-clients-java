443d11b2a534dd890d087b0e8c9579af39d984b9
Dariusz Kaczynski <dkac@softwareag.com>
2023-05-09 15:08:35 +0200

    MTM-53340 Removes org.json:json due to CVE-2022-45688

--- java-client/src/main/java/com/cumulocity/sdk/client/notification/MessageExchange.java
+++ java-client/src/main/java/com/cumulocity/sdk/client/notification/MessageExchange.java
@@ -25,14 +25,14 @@ import org.cometd.client.transport.TransportListener;
 import org.cometd.common.TransportException;
 import javax.ws.rs.core.Response;
 
-import org.json.JSONArray;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.svenson.JSON;
+import org.svenson.JSONParser;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.InvocationCallback;
-import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.ParseException;
@@ -271,11 +271,11 @@ class MessageExchange {
          * continue when single message parsing fail
          */
         private boolean retryHandleContent(String content) {
-            JSONArray jsonArray = new JSONArray(content);
-            List<Mutable> messages = new ArrayList<>(jsonArray.length());
+            List<?> jsonArray = JSONParser.defaultJSONParser().parse(List.class, content);
+            List<Mutable> messages = new ArrayList<>(jsonArray.size());
             for (Object jsonObject : jsonArray) {
                 try {
-                    messages.addAll(transport.parseMessages(jsonObject.toString()));
+                    messages.addAll(transport.parseMessages(JSON.defaultJSON().forValue(jsonObject)));
                 } catch(ParseException | IllegalArgumentException e) {
                     log.debug("Failed to retry parse json message: {}", e.getMessage());
                 }
