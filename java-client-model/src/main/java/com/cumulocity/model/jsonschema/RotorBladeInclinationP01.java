34c0dee8a2ad9c9acf210aca77c57eaff19f9c06
ashh <ashh@softwareag.com>
2023-07-06 11:53:22 +0530

    Add javax validation annotations

--- java-client-model/src/main/java/com/cumulocity/model/jsonschema/RotorBladeInclinationP01.java
+++ java-client-model/src/main/java/com/cumulocity/model/jsonschema/RotorBladeInclinationP01.java
@@ -1,9 +1,16 @@
 package com.cumulocity.model.jsonschema;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+
 public class RotorBladeInclinationP01{
     public String type;
     public String description;
+    @Pattern(regexp = ".+@.+\\\\.[a-z]+")
     public String name;
+    @Min(0)
     public int minimum;
+    @Max(45)
     public int maximum;
 }
