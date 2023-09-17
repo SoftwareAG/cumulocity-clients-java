dcb9462b0936cd2e5e65579447ab4fb306302625
Tomasz Jegorow <tomasz.jegorow@softwareag.com>
2023-09-17 22:07:58 +0200

    MTM-55422 add realtime operation integration tests

--- java-client/src/integration-test/java/com/cumulocity/sdk/client/notification/wrappers/RealtimeOperationMessage.java
+++ java-client/src/integration-test/java/com/cumulocity/sdk/client/notification/wrappers/RealtimeOperationMessage.java
@@ -1,11 +1,12 @@
 package com.cumulocity.sdk.client.notification.wrappers;
 
 import com.cumulocity.rest.representation.AbstractExtensibleRepresentation;
+import com.cumulocity.rest.representation.operation.OperationRepresentation;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class RealtimeDeleteRepresentationWrapper extends AbstractExtensibleRepresentation {
-    String data;
+public class RealtimeOperationMessage extends AbstractExtensibleRepresentation {
+    OperationRepresentation data;
 }
