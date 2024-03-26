bb43cd5cbd89af2b395cb155af6ff1d38772374d
Paweł Kowalczyk (Cumulocity IoT RdepartmentD - Externals) <pawel.kowalczyk@softwareag.com>
2024-03-26 09:55:27 +0100

    [MTM-58527] unpack_artifacts.sh: skip host key verification, remove awk as it swallows error code
    (cherry picked from commit 9e6b63d44909b751c4672a164b613c1edaedcda8)

--- .jenkins/scripts/unpack_artifacts.sh
+++ .jenkins/scripts/unpack_artifacts.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -xe
 
+set -o pipefail # fail on the first command when using bash's pipe |
+
 RELEASE_TYPE=$1
 shift 1
 
-RESOURCES_SERVER=hudson@staging-resources.cumulocity.com
+RESOURCES_SSH_ARGS=" -o StrictHostKeyChecking=no hudson@staging-resources.cumulocity.com"
 STAGING_RESOURCES_DIR="/var/www/staging-resources/sag/java-sdk/${VERSION}"
 RESOURCES="/resources/maven/repository"
 
@@ -13,38 +15,43 @@ echo "Unpacking the published versions from staging to resources"
 
 # below, 2>&1 keeps the order of output in place. Besides this, it doesn't change much
 # 'awk' will print "[ssh]" before every line in ssh
-ssh ${RESOURCES_SERVER} "bash -xe 2>&1" << DISCONNECT_FROM_RESOURCES_SSH  | awk '{print "[ssh] " $0 }'
+ssh ${RESOURCES_SSH_ARGS} 'bash -xe 2>&1' << DISCONNECT_FROM_RESOURCES_SSH | awk '{print "[ssh] " $0 }'
     # maven-repository
     echo "Publish staging maven-repository-${VERSION}.tar.gz to resources "
-    tar -xvzf ${STAGING_RESOURCES_DIR}/maven-repository-${VERSION}.tar.gz -C /resources/maven/repository/ com
-    tar -xvzf ${STAGING_RESOURCES_DIR}/maven-repository-${VERSION}.tar.gz -C /resources/maven/repository/ c8y
+    # By defualt `tar` extracts files with timestamps (--touch) and proper access rights (--no-overwrite-dir). 
+    # But without sudo we can't always do it, for example there are old dependencies 
+    # that did not change, perhaps set by different user.  
+    # Overall, it doesn't matter to us, so these options are skipping this preservation.
+    tar  --touch --no-overwrite-dir -xvf ${STAGING_RESOURCES_DIR}/maven-repository-${VERSION}.tar.gz --directory /resources/maven/repository/ com
+    tar  --touch --no-overwrite-dir -xvf ${STAGING_RESOURCES_DIR}/maven-repository-${VERSION}.tar.gz --directory /resources/maven/repository/ c8y
 
     # java-client-javadoc
     echo "Publishing staging java-client-${VERSION}-javadoc.jar to resources "
-    mkdir -p /resources/documentation/javasdk/${VERSION}
+    mkdir --parents /resources/documentation/javasdk/${VERSION}
+    # unzip `-o` overwrites files, `-d` is destination
     unzip -o ${STAGING_RESOURCES_DIR}/java-client-${VERSION}-javadoc.jar -d /resources/documentation/javasdk/${VERSION}
 
     # microservice-dependencies-javadoc
     echo "Publishing staging microservice-dependencies-${VERSION}-javadoc.jar to resources "
-    mkdir -p /resources/documentation/microservicesdk/${VERSION}
+    mkdir --parents /resources/documentation/microservicesdk/${VERSION}
     unzip -o ${STAGING_RESOURCES_DIR}/microservice-dependencies-${VERSION}-javadoc.jar -d /resources/documentation/microservicesdk/${VERSION}
 
     # lpwan-backend-javadoc
     echo "Publishing staging lpwan-backend-${VERSION}-javadoc.jar to resources "
-    mkdir -p /resources/documentation/lpwan-backend/${VERSION}
+    mkdir --parents /resources/documentation/lpwan-backend/${VERSION}
     unzip -o ${STAGING_RESOURCES_DIR}/lpwan-backend-${VERSION}-javadoc.jar -d /resources/documentation/lpwan-backend/${VERSION}
 
     if [ "release" == "${RELEASE_TYPE}" ]; then
       echo "Update current symbolic link of javasdk javadocs"
-      rm -f /resources/documentation/javasdk/current
-      ln -s /resources/documentation/javasdk/${VERSION} /resources/documentation/javasdk/current
+      rm --force /resources/documentation/javasdk/current
+      ln --symbolic /resources/documentation/javasdk/${VERSION} /resources/documentation/javasdk/current
 
       echo "Update current symbolic link of microservicesdk javadocs"
-      rm -f /resources/documentation/microservicesdk/current
-      ln -s /resources/documentation/microservicesdk/${VERSION} /resources/documentation/microservicesdk/current
+      rm --force /resources/documentation/microservicesdk/current
+      ln --symbolic /resources/documentation/microservicesdk/${VERSION} /resources/documentation/microservicesdk/current
 
       echo "Update current symbolic link of lpwan-backend javadocs"
-      rm -f /resources/documentation/lpwan-backend/current
-      ln -s /resources/documentation/lpwan-backend/${VERSION} /resources/documentation/lpwan-backend/current
+      rm --force /resources/documentation/lpwan-backend/current
+      ln --symbolic /resources/documentation/lpwan-backend/${VERSION} /resources/documentation/lpwan-backend/current
     fi
 DISCONNECT_FROM_RESOURCES_SSH
