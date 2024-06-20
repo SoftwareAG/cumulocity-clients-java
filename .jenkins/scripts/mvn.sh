b2429303efb9c66c18cc2631661f6c9d96e08378
Dariusz Kaczynski <dkac@softwareag.com>
2024-06-20 15:20:07 +0200

    Adapts mvn.sh to Maven 3.9+. Updates maven plugins. (#74)

--- .jenkins/scripts/mvn.sh
+++ .jenkins/scripts/mvn.sh
@@ -2,7 +2,7 @@
 
 # Allows to run Maven build, with additional parameters specified using environmental variables
 
-MAVEN_ARGS="${MAVEN_ARGS:-} ${@}"
+MAVEN_CLI_ARGS="${MAVEN_CLI_ARGS:-} ${@}"
 VERBOSE=true
 FILE=false
 
@@ -15,46 +15,47 @@ while [ $# -gt 0 ]; do
 done
 
 if [ -n "${ADMIN_CREDENTIALS}" ]; then
-  MAVEN_ARGS="--define \"cumulocity.management.username=${ADMIN_CREDENTIALS_USR}\" ${MAVEN_ARGS}"
-  MAVEN_ARGS="--define \"cumulocity.management.password=${ADMIN_CREDENTIALS_PSW}\" ${MAVEN_ARGS}"
+  MAVEN_CLI_ARGS="--define \"cumulocity.management.username=${ADMIN_CREDENTIALS_USR}\" ${MAVEN_CLI_ARGS}"
+  MAVEN_CLI_ARGS="--define \"cumulocity.management.password=${ADMIN_CREDENTIALS_PSW}\" ${MAVEN_CLI_ARGS}"
 fi
 
 if [ -n "${VERSION}" ]; then
-  MAVEN_ARGS="--define revision=${VERSION} ${MAVEN_ARGS}"
+  MAVEN_CLI_ARGS="--define revision=${VERSION} ${MAVEN_CLI_ARGS}"
   if [ -n "${CHANGE_VERSION}" ]; then
-    MAVEN_ARGS="--define changelist=${CHANGE_VERSION} ${MAVEN_ARGS}"
+    MAVEN_CLI_ARGS="--define changelist=${CHANGE_VERSION} ${MAVEN_CLI_ARGS}"
   else
-    MAVEN_ARGS="--define changelist= ${MAVEN_ARGS}"
+    MAVEN_CLI_ARGS="--define changelist= ${MAVEN_CLI_ARGS}"
   fi
 else
   if [ -n "${CHANGE_VERSION}" ]; then
-    MAVEN_ARGS="--define changelist=${CHANGE_VERSION} ${MAVEN_ARGS}"
+    MAVEN_CLI_ARGS="--define changelist=${CHANGE_VERSION} ${MAVEN_CLI_ARGS}"
   fi
 fi
 
 MAVEN_PROFILES="${MAVEN_PROFILES:-ci}"
-MAVEN_ARGS="--activate-profiles $MAVEN_PROFILES ${MAVEN_ARGS}"
+MAVEN_CLI_ARGS="--activate-profiles $MAVEN_PROFILES ${MAVEN_CLI_ARGS}"
 
 if [ -n "$WORKSPACE" ]; then
-  MAVEN_ARGS="--define \"maven.repo.local=${WORKSPACE}/.m2/repository\" ${MAVEN_ARGS}"
+  MAVEN_CLI_ARGS="--define \"maven.repo.local=${WORKSPACE}/.m2/repository\" ${MAVEN_CLI_ARGS}"
 fi
 
 MVN_SETTINGS="${MVN_SETTINGS:-$HOME/.m2/settings.xml}"
-MAVEN_ARGS="--settings $MVN_SETTINGS ${MAVEN_ARGS}"
+MAVEN_CLI_ARGS="--settings $MVN_SETTINGS ${MAVEN_CLI_ARGS}"
 
 if [ $VERBOSE = true ]; then
-  MAVEN_ARGS="--show-version --errors ${MAVEN_ARGS}"
+  MAVEN_CLI_ARGS="--show-version --errors ${MAVEN_CLI_ARGS}"
   set -x
 fi
 
+# https://maven.apache.org/configure.html
 export MAVEN_OPTS="-Xms256m -Xmx512m -XX:MetaspaceSize=96m -XX:MaxMetaspaceSize=128m ${MAVEN_OPTS}"
 
 if [ $FILE = true ]; then # run specified file only
-  ./mvnw --batch-mode ${MAVEN_ARGS}
+  ./mvnw --batch-mode ${MAVEN_CLI_ARGS}
 else # run all contained sub-projects
   for project in '.' 'microservice' 'lpwan-backend' 'cumulocity-sdk'; do
   if [ -d "$project" ]; then
-    ./mvnw --batch-mode --file ${project} ${MAVEN_ARGS}
+    ./mvnw --batch-mode --file ${project} ${MAVEN_CLI_ARGS}
   fi
 done
 fi
